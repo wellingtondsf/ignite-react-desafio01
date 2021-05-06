8e68b356da8f0fd6d4129ea20289f8ecf5e14451
wellingtondsf <wellington@bridge.ufsc.br>
2021-05-06 17:52:22 -0300

    Criacao das funcoes de criar uma tarefa, marcar como resolvida e excluir tarefa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const newTask: Task = {
+        id: Math.floor(Math.random() * 100),
+        title: newTaskTitle,
+        isComplete: false,
+      };
+
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTaskList = tasks.map((task) => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList = tasks.filter((task) => task.id != id);
+
+    setTasks(newTaskList);
   }
 
   return (
